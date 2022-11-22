7eee5c58f0b93981f02d6ce66775127f54f20f11
TailsTek <60035122+TailsTek@users.noreply.github.com>
2022-11-22 18:53:44 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Bot } = require('grammy')
 
 const bot = new Bot('5823745441:AAHgLomjQ-iHPs85VS_Utl2OIp2zImB5s2U');
 
-bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+bot.on("message:text", (ctx) => {
+    ctx.reply("Echo: " + ctx.message.text)
+});
 
 bot.start();
