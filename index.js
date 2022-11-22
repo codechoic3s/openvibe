f64315a3f65f59511bd9bc4fc671785a9904d95b
Lotsmon <anatolii.sokolov.04@mail.ru>
2022-11-22 18:49:45 +1000

    q

---
+++ index.js
@@ -0,0 +1,7 @@
+const { Bot } = require('grammy')
+
+const bot = new Bot('5823745441:AAHgLomjQ-iHPs85VS_Utl2OIp2zImB5s2U');
+
+bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+
+bot.start();
